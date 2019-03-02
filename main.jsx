467bf41d68b9c761e585434dc904534b14e817f8
maneeshakalpani <maneshmkw@gmail.com>
2019-03-02 09:55:32 -0800

    message

---
+++ main.jsx
@@ -0,0 +1,10 @@
+'use strict'
+
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import Appcontainer from './Appcontainer';
+render(<Appcontainer/>,document.getElementById('app'));
+
+
